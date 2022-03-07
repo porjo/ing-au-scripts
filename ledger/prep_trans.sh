8063499c2ac8ee5ab8bfe390f2ecac8294633773
Ian Bishop <ianbishop@pace7.com>
2022-03-08 09:21:16 +1100

    Add ledger bash script

---
+++ ledger/prep_trans.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Make ING transaction file CSV suitable for https://github.com/howeyc/ledger import e.g.
+#  $ ./ledger -f ledger.dat import --date-format "02/01/2006" Paywave trans.csv >> ledger.dat
+
+[ $# -ne 1 ] && echo "Usage $0 <transaciton.csv>" && exit
+
+cat $1 | sed 's/^Date,/Transaction Date,/' | sed 's/Credit,Debit/Amount/' | sed 's/,,/,/g'
